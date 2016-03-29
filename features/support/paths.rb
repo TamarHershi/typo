f4b0c33c2d3a7763b5711842eab1371de79f7040
Tammy <misstamar@gmail.com>
2016-03-29 10:30:06 -0700

    my rspec and cucumber test are passing

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -18,7 +18,7 @@ module NavigationHelpers
     when /^the new article page$/
       '/admin/content/new'
     when /^the new category page$/
-      '/categories/new'
+      '/admin/categories/new'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
